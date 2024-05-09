e77905b3eacd2020e8999e9a86f7e0dda9ed825f
vuong.bui1 <vuong.bui@techvsi.com>
2024-05-10 00:14:35 +0700

    .

---
+++ frontend/dist/assets/useMail-CPWLrai-.js
@@ -0,0 +1,1 @@
+import{e as i}from"./actions-BzfzMeGQ.js";import{aV as n,b8 as r}from"./index-D5NEVckz.js";import{h as c}from"./selectors-RESdbiRT.js";function d({entity:s}){const{isLoading:t}=n(c),o=r();return{send:a=>{const e={id:a};o(i.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
--- frontend/dist/assets/useMail-tQVLgNkB.js
+++
@@ -1,1 +0,0 @@
-import{e as i}from"./actions-C54H2QhY.js";import{aV as n,b8 as r}from"./index-Bv18HJQU.js";import{h as c}from"./selectors-DEj3Vb5e.js";function d({entity:s}){const{isLoading:t}=n(c),o=r();return{send:a=>{const e={id:a};o(i.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
